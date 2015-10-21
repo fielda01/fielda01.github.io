f6ca9a2dc72d0a35a4a06700ee364c1bb9b18d34
fielda01 <fielda01@luther.edu>
2015-10-21 13:55:50 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,34 @@
-asjdflkasldkfal;sdf
+addtask = function() {
+  var tasklist = document.querySelector('#tasklist');
+  var li = document.createElement("li");
+  
+  var cb = document.createElement("input");
+  cb.setAttribute("type", "checkbox");
+  cb.onclick = doneTask;
+  
+  var task = document.querySelector("#abc").value;
+  var litext = document.createTextNode(task);
+  
+  li.appendChild(cb);
+  li.appendChild(litext);
+  li.className = document.querySelector("#priority").value;
+  tasklist.appendChild(li);
+  document.body.appendChild(tasklist);
+  
+  
+  //priority = document.querySelector('#priority')
+  //li.className = priority.value
+  
+
+}  
+  //mycb.onclick = boxChecked;
+  //this.parentNode.classList.add("done")
+  
+doneTask = function() {
+  if (this.checked) {
+  this.parentNode.classList.add("done");
+  } else {
+    this.parentNode.classList.remove("done");
+  }
+  
+}
